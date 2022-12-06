1ff09d495fee661f1c1943b1890cc5ed9cdfed58
Andreas Erdes <socialcoms@posteo.de>
2022-12-06 15:48:47 +0100

    Add spring-boot-3-client project

---
+++ spring-boot-3-client/src/main/java/com/example/springboot3client/SpringBoot3ClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ClientApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringBoot3ClientApplication.class, args);
+    }
+
+}
---
+++ spring-boot-3-client/src/test/java/at/openvalue/springboot3client/SpringBoot3ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package at.openvalue.springboot3client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
