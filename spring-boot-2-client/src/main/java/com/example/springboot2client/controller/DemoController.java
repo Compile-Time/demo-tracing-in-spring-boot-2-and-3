e140ad38f73c0887a13b3648928646444d2b6260
Andreas Erdes <socialcoms@posteo.de>
2022-12-06 15:11:26 +0100

    Implement demo for `@NewSpan` annotation usage

--- spring-boot-2-client/src/main/java/com/example/springboot2client/controller/DemoController.java
+++ spring-boot-2-client/src/main/java/com/example/springboot2client/controller/DemoController.java
@@ -1,16 +1,15 @@
 package com.example.springboot2client.controller;
 
+import com.example.commoninterface.user.User;
 import com.example.commoninterface.user.UserCreationRequest;
 import com.example.commoninterface.user.UserCreationResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
@@ -21,12 +20,14 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class DemoController {
 
+    private static final String LOCAL_HOST = "http://localhost:8080";
+
     private final Tracer tracer;
     private final RestTemplate restTemplate;
 
     private ResponseEntity<UserCreationResult> requestUserCreation(final String restEndpoint, final UserCreationRequest creationRequest) {
         log.info("Creating new user (with group) with the following data: {}", creationRequest);
-        final var requestUrl = String.format("http://localhost:8080/%s", restEndpoint);
+        final var requestUrl = String.format("%s/%s", LOCAL_HOST, restEndpoint);
 
         final var response = restTemplate.postForEntity(requestUrl, creationRequest, UserCreationResult.class);
         Optional.ofNullable(response.getBody())
@@ -52,5 +53,23 @@ public class DemoController {
         Optional.ofNullable(this.tracer.currentSpan())
                 .ifPresent(span -> span.event("request new user for tracer with current span sample"));
         return requestUserCreation("tracer/users/current-span", creationRequest);
+    }
+
+    @PostMapping("new-span")
+    @NewSpan(name = "request new user for @NewSpan sample")
+    public ResponseEntity<UserCreationResult> createUser(@RequestBody final UserCreationRequest creationRequest) {
+        return requestUserCreation("new-span/users", creationRequest);
+    }
+
+    @GetMapping("new-span/repository/{userName}")
+    @NewSpan(name = "request new user for @NewSpan sample with repository span")
+    public ResponseEntity<User> createUserRepositorySpan(@PathVariable("userName") final String userName) {
+        log.info("Request user with name '{}'", userName);
+
+        final var userResponse = restTemplate.getForEntity(String.format("%s/%s/%s", LOCAL_HOST, "new-span/users", userName), User.class);
+        Optional.ofNullable(userResponse.getBody())
+                .ifPresent(user -> log.info("Received user {}", user));
+
+        return userResponse;
     }
 }
