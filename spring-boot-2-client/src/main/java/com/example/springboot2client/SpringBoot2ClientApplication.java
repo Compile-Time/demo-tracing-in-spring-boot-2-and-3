8ba8b08109e92ae29f0acc95febfe0045928e300
Andreas Erdes <socialcoms@posteo.de>
2022-12-06 08:27:58 +0100

    Add spring-boot-2-client project

---
+++ spring-boot-2-client/src/main/java/com/example/springboot2client/SpringBoot2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2ClientApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringBoot2ClientApplication.class, args);
+    }
+
+}
---
+++ spring-boot-2-client/src/test/java/com/example/springboot2client/SpringBoot2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
